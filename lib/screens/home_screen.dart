eb18aaad5cb33bb37ec951a8a1cadc31b95504ac
nico-develop <contact@nico-develop.fr>
2022-10-08 08:06:26 +0200

    Permet de fermer tous les écrans (formulaire) quand on clique sur un menu - fixed #1

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,7 +4,9 @@ import "package:com_noopeshop_backend/components/sidebar/menu_sidebar_component.
 import "package:com_noopeshop_backend/config/constants.dart";
 import "package:com_noopeshop_backend/config/forms/user.dart";
 import "package:com_noopeshop_backend/screens/page_screen.dart";
+import "package:com_noopeshop_backend/services/menu/click_menu/click_menu_bloc.dart";
 import "package:flutter/material.dart";
+import "package:flutter_bloc/flutter_bloc.dart";
 import "package:flutter_speed_dial/flutter_speed_dial.dart";
 
 class HomeScreen extends StatefulWidget {
@@ -21,56 +23,61 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(
-            height: double.infinity,
-            width: 300,
-            child: MenuSidebasComponent(),
-          ),
-          Expanded(
-            child: Stack(
-              children: [
-                const PageScreen(),
-                Visibility(
-                  visible: _formVisible,
-                  child: Positioned(
-                    top: 0,
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    child: WrapperFormComponent(
-                      onClose: () => setState(
-                        () => _formVisible = false,
-                      ),
-                      child: FormGeneratorComponent(
-                        formData: userForm,
+    return BlocListener<ClickMenuBloc, ClickMenuState>(
+      listener: (context, state) {
+        setState(() => _formVisible = false);
+      },
+      child: Scaffold(
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: double.infinity,
+              width: 300,
+              child: MenuSidebasComponent(),
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  const PageScreen(),
+                  Visibility(
+                    visible: _formVisible,
+                    child: Positioned(
+                      top: 0,
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: WrapperFormComponent(
+                        onClose: () => setState(
+                          () => _formVisible = false,
+                        ),
+                        child: FormGeneratorComponent(
+                          formData: userForm,
+                        ),
                       ),
                     ),
-                  ),
-                )
-              ],
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
-      floatingActionButton: SpeedDial(
-        icon: Icons.add,
-        spacing: kDefaultPadding,
-        children: [
-          SpeedDialChild(
-            child: const Icon(
-              Icons.person_add,
+          ],
+        ),
+        floatingActionButton: SpeedDial(
+          icon: Icons.add,
+          spacing: kDefaultPadding,
+          children: [
+            SpeedDialChild(
+              child: const Icon(
+                Icons.person_add,
+              ),
+              label: "Create user",
+              onTap: () {
+                setState(() => _formVisible = true);
+              },
             ),
-            label: "Create user",
-            onTap: () {
-              setState(() => _formVisible = true);
-            },
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
